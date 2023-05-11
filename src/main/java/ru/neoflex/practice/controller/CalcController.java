2dc6e1faee7ad520ab31f4968a8e8c11d00f4428
DJ-Von <kotruntsev.gleb@yandex.ru>
2023-05-11 13:08:55 +0300

    Commit calculator first version

---
+++ src/main/java/ru/neoflex/practice/controller/CalcController.java
@@ -0,0 +1,44 @@
+package ru.neoflex.practice.controller;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Описание контроллера CalcController",
+                description = "Практическое задание",
+                contact = @Contact(
+                        name = "Котрунцев Глеб",
+                        email = "kotruntsev.gleb@yandex.ru",
+                        url = "https://github.com/DJ-Von"
+                )
+        )
+)
+@Tag(name = "Калькулятор", description = "Позволяет складывать и вычитать числа")
+public class CalcController {
+    @GetMapping("/plus/{a}/{b}")
+    @Operation(
+            summary = "Сложение",
+            description = "Позволяет вычислить сумму двух чисел"
+    )
+    public int plus(@PathVariable int a,
+                    @PathVariable int b){
+        return a+b;
+    }
+    @GetMapping("/minus/{a}/{b}")
+    @Operation(
+            summary = "Вычитание",
+            description = "Позволяет вычислить разность двух чисел"
+    )
+    public int minus(@PathVariable int a,
+                     @PathVariable int b){
+        return a-b;
+    }
+}
